e1b4dd307699d54c9da491c80ea0bae6a1cee368
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-10-19 13:34:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 import pygame
-from player import Player
 import functions
-from blocs import Block
 import math
+import settings
+from player import Player
 from ball import Ball
 
 pygame.init()
-size = width, height = 800, 600
+player = Player(settings.screen)
+ball = Ball(settings.screen, player)
 
-grey = 230, 230, 230
-
-screen = pygame.display.set_mode(size)
-screen.fill(grey)
-pygame.display.set_caption("Arkanoid")
-player = Player(screen)
-ball = Ball(screen, player)
-blox = [Block(screen, 21)]
-
-for i in range(1, 11):
-    blox.append(Block(screen, blox[-1].rect.right + 10))
 while True:
     functions.check_events(player, ball)    
-    screen.fill(grey)
+    settings.screen.fill(settings.background_color)
     player.update()
-    ball.update(blox, player)
-    dead = []    
-    for i in range(len(blox)):
-        blox[i].blitme()
-        if blox[i].life == 0 :
-            dead.append(i)
-    for d in dead:
-        blox.pop(d)
+    ball.update(settings.levels[0], player)
+    functions.update_level(settings.levels[0])
     pygame.display.flip()
-
